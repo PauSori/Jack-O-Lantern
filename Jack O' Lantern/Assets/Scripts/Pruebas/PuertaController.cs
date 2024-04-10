6b91faf92a4533d13704850f83c9bb5b52056d52
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2024-04-10 20:00:07 +0200

    Reset de enemigos al acercarte a la puerta de la taberna

--- Jack O' Lantern/Assets/Scripts/Pruebas/PuertaController.cs
+++ Jack O' Lantern/Assets/Scripts/Pruebas/PuertaController.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PuertaController : MonoBehaviour
 {
+    private bool shouldResetEnemies = false;
+
     private bool cameraDisabled = false;
 
     public Camera Camera;
     public static PuertaController instance;
     private Animator animator;
     public List<GameObject> enemies = new List<GameObject>();
+
+    private CapsuleCollider capsuleCollider;
+
+    public GameObject enemy;
+    public int numberOfEnemiesToSpawn = 5;
+    public float spawnRadius = 10f; // Radio de generación
+    public Transform spawnPoint; // Punto de origen (por ejemplo, el objeto que representa el suelo)
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        Vector2 randomCircle = Random.insideUnitCircle * spawnRadius;
+        Vector3 spawnPosition = new Vector3(randomCircle.x, 0f, randomCircle.y) + spawnPoint.position;
+        return spawnPosition;
+    }
     public void Awake()
     {
         instance = this;
@@ -17,6 +33,7 @@ public class PuertaController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         Camera.gameObject.SetActive(false);
         animator = GetComponent<Animator>();
     }
@@ -27,8 +44,13 @@ public class PuertaController : MonoBehaviour
         if (!cameraDisabled && enemies.Count <= 0)
         {
             Camera.gameObject.SetActive(true);
+            capsuleCollider.enabled = false;
             animator.SetBool("Open", true);
             Invoke("CameraOff", 5f);
+        }
+        if (shouldResetEnemies == true)
+        {
+            ResetEnemies();
         }
 
     }
@@ -44,7 +66,34 @@ public class PuertaController : MonoBehaviour
     {
         enemies.Remove(obj);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldResetEnemies = true;
+            capsuleCollider.enabled = false;
+        }
+    }
 
+    public void ResetEnemies()
+    {
+        Debug.Log("Reset de enemigos");
+        foreach (var enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+        enemies.Clear();
+
+        // Genera nuevos enemigos
+        for (int i = 0; i < numberOfEnemiesToSpawn; i++)
+        {
+            Vector3 spawnPosition = GetRandomSpawnPosition(); // Implementa tu lógica para obtener una posición de generación
+            GameObject newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
+            enemies.Add(newEnemy);
+        }
+
+        shouldResetEnemies = false;
+    }
     // Llama a la coroutine cuando sea necesario
 
 }
