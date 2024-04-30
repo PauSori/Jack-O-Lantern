61f6cc9018d77b8f7a680b37a7f5b52caf9043b8
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2024-05-01 01:21:39 +0200

    Jack final hecho

--- Jack O' Lantern/Assets/Scripts/Jack/Managers/JackTrigger.cs
+++ Jack O' Lantern/Assets/Scripts/Jack/Managers/JackTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class JackTrigger : MonoBehaviour
 {
+    public GameObject final;
     public GameObject jack;
     public GameObject tombs;
     // Start is called before the first frame update
@@ -11,12 +12,18 @@ public class JackTrigger : MonoBehaviour
     {
         jack.SetActive(false);
         tombs.SetActive(false);
+        final.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (jack == null)
+        {
+            final.SetActive (true);
+            Time.timeScale = 0.0f;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
